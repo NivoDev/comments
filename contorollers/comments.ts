c49104d2f84162cc932453cc3e44ea9d5ffce2c2
niv <nivroz93@gmail.com>
2021-03-24 20:59:34 +0200

    add comments controller

--- contorollers/comments.ts
+++ contorollers/comments.ts
@@ -1,7 +1,12 @@
 import Comment from '../models/comment';
 
-export const getComments = (req,res) => {
-   Comment.find(req.query.entity)
+export const getComments = async(req,res) => {
+    const entity = req.query.entity;
+    if(!entity){
+        return res.status(400).json({message: "Please provide an entity ID"})
+    }
+    const comments = await Comment.find({entity});
+    res.json(comments);
 }
 
 export const createComment = (req,res) => {
--- models/comment.ts
+++ models/comment.ts
@@ -1,7 +1,11 @@
 import {model, Schema, Types} from 'mongoose';
 
 const CommentSchema = new Schema({
-	
+    entity: {
+        type: String,
+        index: true,
+        required: true
+    }
 });
 
 const Comment = model('Comment', CommentSchema);
